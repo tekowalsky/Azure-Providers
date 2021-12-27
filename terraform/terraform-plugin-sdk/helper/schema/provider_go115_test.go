355da317126f50492cbdba6b5334d9ef9d503c53
getHashiSchemas-Workflow <github-actions@github.com>
2021-12-27 19:39:05 +0000

    Commit by getHashiSchemas workflow

---
+++ terraform/terraform-plugin-sdk/helper/schema/provider_go115_test.go
@@ -0,0 +1,6 @@
+//go:build go1.15
+// +build go1.15
+
+package schema
+
+const invalidDurationErrMsg = "time: invalid duration \"invalid\""
---
+++ terraform/terraform-plugin-sdk/tools/tools.go
@@ -0,0 +1,10 @@
+//go:build tools
+// +build tools
+
+package tools
+
+import (
+	_ "github.com/golang/mock/mockgen"
+	_ "golang.org/x/tools/cmd/cover"
+	_ "golang.org/x/tools/cmd/stringer"
+)
